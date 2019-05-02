53a784f0e06ebc7a701e4d953d1ac90e38304ce8
mb2o <mboomaars@gmail.com>
2019-05-02 21:03:19 +0200

    Typechecking With PropTypes

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 const Header = ({ branding }) => {
   return (
@@ -6,6 +7,14 @@ const Header = ({ branding }) => {
       <h1>{branding}</h1>
     </div>
   );
+};
+
+Header.propTypes = {
+  branding: PropTypes.string.isRequired
+};
+
+Header.defaultProps = {
+  branding: 'My App'
 };
 
 export default Header;
