60d402856588882ba421e7a0c45a5c366f87aa8b
mb2o <mboomaars@gmail.com>
2019-05-03 23:38:21 +0200

    Error Checking & Display

--- src/components/contacts/AddContact.js
+++ src/components/contacts/AddContact.js
@@ -8,13 +8,27 @@ class AddContact extends Component {
   state = {
     name: '',
     email: '',
-    phone: ''
+    phone: '',
+    errors: {}
   };
 
   onSubmit = (dispatch, e) => {
     e.preventDefault();
 
     const { name, email, phone } = this.state;
+
+    if (name === '') {
+      this.setState({ errors: { name: 'Name is required' } });
+      return;
+    }
+    if (email === '') {
+      this.setState({ errors: { email: 'Email is required' } });
+      return;
+    }
+    if (phone === '') {
+      this.setState({ errors: { phone: 'Phone is required' } });
+      return;
+    }
 
     dispatch({
       type: 'ADD_CONTACT',
@@ -29,14 +43,15 @@ class AddContact extends Component {
     this.setState({
       name: '',
       email: '',
-      phone: ''
+      phone: '',
+      errors: {}
     });
   };
 
   onChange = e => this.setState({ [e.target.name]: e.target.value });
 
   render() {
-    const { name, email, phone } = this.state;
+    const { name, email, phone, errors } = this.state;
 
     return (
       <Consumer>
@@ -54,6 +69,7 @@ class AddContact extends Component {
                     placeholder="Enter name"
                     value={name}
                     onChange={this.onChange}
+                    error={errors.name}
                   />
 
                   <TextInputGroup
@@ -63,6 +79,7 @@ class AddContact extends Component {
                     placeholder="Enter email"
                     value={email}
                     onChange={this.onChange}
+                    error={errors.email}
                   />
 
                   <TextInputGroup
@@ -71,6 +88,7 @@ class AddContact extends Component {
                     placeholder="Enter phone"
                     value={phone}
                     onChange={this.onChange}
+                    error={errors.phone}
                   />
 
                   <input
