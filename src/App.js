3091e329e2359392fba45a94cced5939486f88b6
mb2o <mboomaars@gmail.com>
2019-05-03 18:03:30 +0200

    Creating A Form With State

--- src/App.js
+++ src/App.js
@@ -6,12 +6,14 @@ import React from 'react';
 import Contacts from './components/contacts/Contacts';
 import Header from './components/layout/Header';
 import { Provider } from './context';
+import AddContact from './components/contacts/AddContact';
 
 const App = () => (
   <Provider>
     <div>
       <Header branding="Contact Manager" />
       <div className="container">
+        <AddContact />
         <Contacts />
       </div>
     </div>
